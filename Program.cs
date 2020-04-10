3d7e5be9caa37e1890b02c0546ed3a2bf15bdac8
Кодиров Султон <sultonkodiro109@gmail.com>
2020-04-10 22:04:40 +0500

    Добавлен базовой класс DocumentWorker и два производных классов Pro и Exp

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW_09_04_task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string pro = "123pro";
+            const string exp = "123exp";
+            
+            
+        }
+    }
+    public class DocumentWorker{
+
+        public void OpenDocument(){
+            System.Console.WriteLine("Документ открыть");
+        }
+        public virtual void EditDocument(){
+            System.Console.WriteLine("Редактирование документа доступно в версии Про");
+        }
+        public virtual void SaveDocument(){
+            System.Console.WriteLine("Сохранение документа доступно в Про");
+        }
+    }
+
+    class ProDocumentWorker : DocumentWorker{
+        public override void EditDocument(){
+            System.Console.WriteLine("Документ отредактирован");
+        }
+        public override void SaveDocument(){
+            System.Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+
+    }
+    class ExpertDocumentWorker : ProDocumentWorker{
+        public override void SaveDocument(){
+            System.Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
+
+
+}
